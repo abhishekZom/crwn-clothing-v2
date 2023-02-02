d1905a49b7f5249c6e8feb93ee4e75f94ae6c42f
Abhishek Kumar <abhi28k09@gmail.com>
2023-02-02 20:14:50 +0530

    routing and navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import Directory from './components/Directory';
+import { Route, Routes } from 'react-router-dom';
+import Home from './routes/Home/Home';
+import Navigation from './routes/Navigation';
+
+
+const Shop = () => {
+  return (
+    <div>
+      I am shop
+    </div>
+  )
+}
 
 const App = () => {
-  return (<Directory/> );
+  return (
+    <Routes>
+      <Route path="/" element={ <Navigation /> }>
+        <Route index element={ <Home /> } />
+        <Route path="shop" element={ <Shop /> } />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
